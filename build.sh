9659e02c8e6c136dae7ddf6f2808fb4e14f8951b
Bit D13 <bit@d13.co>
2025-05-16 11:10:46 +0300

    build

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 if [ "$CF_PAGES_BRANCH" = "main" ]; then
   hugo --baseURL "https://d13.co"
+elif [[ "$CF_PAGES_BRANCH" =~ ^testnet ]]; then
+  hugo --baseURL "https://testnet.d13-co.pages.dev"
 else
   hugo --baseURL "$CF_PAGES_URL"
 fi
